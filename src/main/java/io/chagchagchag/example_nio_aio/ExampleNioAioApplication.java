376b6012886d84f452d875562a2514aacc92f20e
chagchagchag <chagchagchag.dev@gmail.com>
2024-04-16 20:54:08 +0900

    first status

---
+++ src/main/java/io/chagchagchag/example_nio_aio/ExampleNioAioApplication.java
@@ -0,0 +1,13 @@
+package io.chagchagchag.example_nio_aio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleNioAioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleNioAioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/chagchagchag/example_nio_aio/ExampleNioAioApplicationTests.java
@@ -0,0 +1,13 @@
+package io.chagchagchag.example_nio_aio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleNioAioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
